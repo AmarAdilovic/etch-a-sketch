3935a7ea97dffc26d62e265242197a595e3705e2
AmarAdilovic <amaradilovic13@gmail.com>
2022-04-08 12:53:54 -0400

    Improve pixel grid and add drawing functionality

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ function htmlElements(){
     const container = document.querySelector(".container");
 
     const headerContainer = document.createElement("div");
+    const headerText = document.createElement("h1");
+    headerText.textContent = "Etch-a-sketch";
     // Contains a grid of "pixels" 
     const gridContainer = document.createElement("div");
     // Each container has 16 div "pixel" elements
@@ -13,19 +15,51 @@ function htmlElements(){
 
         for(let j = 0; j < 16; j++){
             const pixelDiv = document.createElement("div");
-            pixelDiv.textContent = "pixel";
-            pixelDiv.style.color = "blue";
+            pixelDiv.style.height = "15px";
+            pixelDiv.style.width = "15px";
             pixelDiv.style.padding = "2.5px";
+            pixelDiv.style.border = "1.5px solid black";
+            
+
             rowContainer.appendChild(pixelDiv);
         }
         gridContainer.appendChild(rowContainer);
-
     }
+      
+    draw(gridContainer, "black");
+
     gridContainer.style.display = "flex";
     gridContainer.appendChild(rowContainer);
 
+    headerContainer.appendChild(headerText);
+
+    container.style.display = "flex";
+    
     container.appendChild(headerContainer);
     container.appendChild(gridContainer);
+}
+
+function draw(container, color){
+    let isDrawing = false;
+    let x = 0;
+    let y = 0;    
+
+    container.addEventListener('mousedown', e => {
+        isDrawing = true;
+        (e.target).style.backgroundColor = color;
+    });
+      
+    container.addEventListener('mousemove', e => {
+        if (isDrawing === true) {
+            (e.target).style.backgroundColor = color;
+        }
+    });
+    
+    window.addEventListener('mouseup', e => {
+        if (isDrawing === true) {
+          isDrawing = false;
+        }
+    });
 }
 
 
